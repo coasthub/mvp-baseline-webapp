7ff19e5c806bba5d36f3659a3521872de1b3d676
Leonardo Lopes dos Santos <lameranha@gmail.com>
2020-11-23 16:28:22 -0300

    react js basic intro

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import logo from './logo.svg';
+import { useState, useEffect } from 'react'
+import socketIo from 'socket.io-client'
 import './App.css';
 
 function App() {
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3030/sellerOrders", {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({ "sellerId": "5fb99d4a3ef2ad16b444ade0" })
+    })
+      .then((res) => res.json())
+      .then(setOrders)
+
+      const socket = socketIo("http://localhost:3030/", {
+        transports: ['websocket']
+      })
+
+      socket.on('newOrder', (order) => {
+        setOrders((pastOrders) => [order, ...pastOrders])
+      })
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {orders.map(order =>(
+          <p key={order._id}>{order.clientName}</p>
+          // <p>{order.clientAdress}</p>
+
+        ))}
       </header>
     </div>
   );
